66e85da0dc79f15adde87340c336b15a6131c358
AlfonsoNango <molina.nango@gmail.com>
2024-03-05 20:34:11 -0600

    We add all the pages design.

--- src/alldata.jsx
+++ src/alldata.jsx
@@ -5,9 +5,27 @@ export default function AllData() {
   const ctx = useContext(UserContext);
   return (
     <>
-      <h5>All Data in Store</h5>
+      {/* <h5>All Data in Store</h5>
       {JSON.stringify(ctx)}
-      <br />
+      <br /> */}
+
+      <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">Name</th>
+            <th scope="col">Mail</th>
+            <th scope="col">Password</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td scope="row">Name</td>
+            <td>Mark@gmail.com</td>
+            <td>Otto</td>
+          </tr>
+        </tbody>
+      </table>
     </>
   );
 }
